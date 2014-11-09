a51f9ff00f921174822ef3b53624443654e3b49c
Beata Obrok <beata@obrok.eu>
2014-11-09 14:50:27 +0100

    Initialize Gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,15 @@
+
+module.exports = function(grunt) {
+
+  // Load all grunt tasks
+  require('load-grunt-tasks')(grunt);
+
+  // Project configuration
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+  });
+
+  grunt.registerTask('default', []);
+
+};
